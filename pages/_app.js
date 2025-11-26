1b9a6a775043e12e9e7c28bd5a35083084dae89d
Shashank <shashankmn171@gmail.com>
2025-11-26 16:27:21 +0530

    Initia

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ moengage.initialize({
  app_id: 'ILHCGEFZ04ELWYTI71A01OW2', // Your App ID
  cluster: 'DC_3',
  debug_logs: 1,                      // Change to 0 when in LIVE en
+ swPath: "../public/serviceworker.js"
 });
 
   return (
