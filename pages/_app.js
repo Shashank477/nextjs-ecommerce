cccb4aef39f28e517255c2828debab0b819454b6
Shashank <shashankmn171@gmail.com>
2025-11-26 18:27:30 +0530

    Initia

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,14 @@ moengage.initialize({
  debug_logs: 0,                      // Change to 0 when in LIVE en
 });
 
+useEffect(() => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceworker.js')
+      .then(reg => console.log("Service worker registered", reg))
+      .catch(err => console.error("SW registration failed", err));
+  }
+}, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
